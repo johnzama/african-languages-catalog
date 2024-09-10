7ac017dedbaef5485a890643611a5cc70c65b1c5
johnzama <johnsparta2121@gmail.com>
2024-09-10 17:10:44 -0400

    initialcommit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the African Languages Catalog</h1>
+      <p>Explore and learn about the diverse languages of Africa.</p>
+    </div>
+  );
+};
+
+export default Home;
+
